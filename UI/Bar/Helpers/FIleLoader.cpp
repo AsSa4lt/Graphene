8baaf66459b6bfd256a05bdcb61a9991c2558e31
Rostyslav <st1tchqwerty@gmail.com>
2023-08-31 20:37:38 +0200

    Fucking opening Finder works now

--- UI/Bar/Helpers/FIleLoader.cpp
+++ UI/Bar/Helpers/FIleLoader.cpp
@@ -3,14 +3,17 @@
 //
 
 #include "FIleLoader.h"
-
+#include "tinyfiledialogs.h"
+#include "../../../external/portable-file-dialogs.h"
 #include <string>
 #include <TGUI/TGUI.hpp>
 
-namespace fs = std::filesystem;
 
 FileLoader::FileLoader(tgui::Gui& gui, const std::string& extension) : m_gui(gui) {
-
+    auto f = pfd::open_file("Choose files to read", pfd::path::home(),
+                            { "Text Files (.txt .text)", "*.txt *.text",
+                              "All Files", "*" },
+                            pfd::opt::multiselect);
 }
 
 void FileLoader::populateFileList(tgui::ListBox::Ptr listBox, const std::string& extension) {
--- UI/Bar/UIBarFileButton.cpp
+++ UI/Bar/UIBarFileButton.cpp
@@ -4,7 +4,6 @@
 
 #include "UIBarFileButton.h"
 #include "Helpers/FIleLoader.h"
-#include "nfd.h"
 
 
 void UIBarFileButton::actionOnClick() {
